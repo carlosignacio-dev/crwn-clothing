ff65b90b09cd6257719eb573df75224a1b331b43
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-06-18 20:32:02 -0400

    added navigation component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
+
+
 
 const App = () => {
-  return <Home />
+  return (
+    <Routes>  
+      <Route path="/" element={<Navigation />}> 
+        <Route index element={<Home />}/>    
+      </Route>
+    </Routes>
+  );
 }
 
 
