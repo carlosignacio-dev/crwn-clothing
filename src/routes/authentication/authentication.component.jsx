4de99340b28a498173193de110864ccdfe8917c5
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-06-27 16:42:07 -0400

    added sign-in component

--- src/routes/authentication/authentication.component.jsx
+++ src/routes/authentication/authentication.component.jsx
@@ -4,21 +4,18 @@ import {
 } from "../../utils/firebase/firebase.utils";
 
 import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
 
 
-const SignIn = () => {  
+const Authentication = () => {  
 
-    const logGoogleUser = async () => {
-        const { user } = await signInWithGooglePopup();
-        const userDocRef = await createUserDocumentFromAuth(user);
-    }
     return (
       <div>
         <h1>Sign In Page</h1>
-        <button onClick={logGoogleUser}>Sign in with Google PupUp</button>
+        <SignInForm />
         <SignUpForm />
       </div>  
     );
 };
 
-export default SignIn;
+export default Authentication;
--- src/utils/firebase/firebase.utils.jsx
+++ src/utils/firebase/firebase.utils.jsx
@@ -4,7 +4,8 @@ import {
     signInWithRedirect,
     signInWithPopup,
     GoogleAuthProvider,
-    createUserWithEmailAndPassword
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword
 } from "firebase/auth";
 
 import { 
@@ -79,4 +80,10 @@ const firebaseConfig = {
     if(!email || !password) return;
 
     createUserWithEmailAndPassword(auth, email, password);
+  }
+
+  export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+    if(!email || !password) return;
+
+    return await signInWithEmailAndPassword(auth, email, password);
   }
