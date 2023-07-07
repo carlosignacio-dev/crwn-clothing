6dbf6577af2bdc0fe67daa3c41297394b2cc3a8e
Nash-Aud <92648695+Nash-Aud@users.noreply.github.com>
2023-07-07 19:28:05 -0400

    added checkout increment and decrement functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Routes, Route } from "react-router-dom";
 
 import Home from "./routes/home/home.component";
+import Authentication from "./routes/authentication/authentication.component";
 import Navigation from "./routes/navigation/navigation.component";
 import Shop from "./routes/shop/shop.component";
-import Authentication from "./routes/authentication/authentication.component";
+import Checkout from "./routes/checkout/checkout.component";
 
 
 const App = () => {
@@ -12,7 +13,8 @@ const App = () => {
       <Route path="/" element={<Navigation />}> 
         <Route index element={<Home />}/>    
         <Route path="shop" element={<Shop />}/>    
-        <Route path="auth" element={<Authentication />}/>    
+        <Route path="auth" element={<Authentication />} />    
+        <Route path="checkout" element={<Checkout />} />    
       </Route>
     </Routes>
   );
